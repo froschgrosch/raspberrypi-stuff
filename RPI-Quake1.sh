b1c287cd2b8581f96398778492ca159eb00c047b
froschgrosch <65343661+froschgrosch@users.noreply.github.com>
2021-07-03 14:08:14 +0200

    Upload RPI-Quake1

---
+++ RPI-Quake1.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Raspbian Quake3 installer
+# https://github.com/froschgrosch/raspberrypi-stuff
+
+TARGETDIR="/home/pi/quake1"
+SOURCEDIR="/tmp/qurp-source"
+
+# install required
+apt-get update -y || exit 1
+apt-get install git gcc build-essential libsdl2-dev libudev-dev -y || exit 1
+
+# download qurp
+
+
+git clone https://github.com/lasthopegf/qurp.git
+
+# build
+cd qurp/WinQuake
+./build.sh release_gl 
+
+
+copy releasearm/bin/glquake ${TARGETDIR}/glquake
+
+rm -r ${SOURCEDIR}
+
+chown pi ${TARGETDIR} -R
